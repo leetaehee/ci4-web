5fa0d818c35fabf3bf4238a6dbca574097eeeec8
developerkimtakgu <developerkimtakgu@gmail.com>
2023-04-10 12:57:05 +0900

    수정사항 - 복구

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,18 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        $a = 4;
-        $b = 10;
-        $c = 2;
-
-        $dd = ($a + $b) / $c;
-
-        echo $dd . "<br>";
-
-        echo "코드이그나이터4d입니다.. git을 pull 하는 사람은 지워주시길 바랍니다.. <br> ";
-
-        echo "--- end ---";
-
         return view('welcome_message');
     }
 }
